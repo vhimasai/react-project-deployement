51a3255dca4a418c2e5e6d675033722951f3c70f
venkikvr <kvenki1998@gmail.com>
2023-09-27 10:08:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to Home ......</h1>
+        <p>Dev in progress ....</p>
       </header>
     </div>
   );
