d2031a0cbf4122d73db769ad16324651fb3b8571
venkikvr <kvenki1998@gmail.com>
2023-10-06 10:32:47 +0530

    added base url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
-const apiBaseUrl =
-  "http://testvenki.southindia.cloudapp.azure.com:3001/api/v1/";
+const apiBaseUrl = "http://20.235.169.49:3001/api/v1/";
 
 function App() {
   const [tasks, setTasks] = useState([]);
